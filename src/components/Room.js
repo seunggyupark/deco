6edf7e02066d981445e9aee0960d09f93f7e4f42
Dev <park@seung.dev>
2021-01-04 17:34:01 -0800

    Added shadows, lighting, cameras

--- src/components/Room.js
+++ src/components/Room.js
@@ -1,34 +1,44 @@
 import React, { Suspense } from "react";
 import { Canvas } from "react-three-fiber";
-import { OrbitControls, ContactShadows } from "drei";
+import { OrbitControls } from "drei";
 
 import Bedroom from "./Bedroom";
 
+// Learn what those light settings mean
 const Lights = () => {
   return (
     <>
       <ambientLight intensity={0.4} />
-      <directionalLight castShadow position={[0, 35, 35]} intensity={0.5} />
+      <directionalLight
+        castShadow
+        position={[10, 20, 15]}
+        intensity={.8}
+        shadow-mapSize-width={1024}
+        shadow-mapSize-height={1024}
+        shadow-camera-far={55}
+        shadow-camera-left={-50}
+        shadow-camera-right={20}
+        shadow-camera-top={35}
+        shadow-camera-bottom={-35}
+      />
     </>
   );
 };
 
-const HTMLContent = () => {
-  // const { nodes, materials } = useGLTF("chair.glb")
-};
-
 export default function Room() {
   return (
-    <Canvas shadowMap colorManagement camera={{ position: [35, 35, 35], fov: 75 }}>
+    <Canvas shadowMap colorManagement camera={{ position: [35, 20, 35], fov: 75 }}>
       <Lights />
       <Suspense fallback={null}>
         <Bedroom />
       </Suspense>
       <OrbitControls
-        minPolarAngle={Math.PI / 3.3}
-        maxPolarAngle={Math.PI / 3.3}
-        enableZoom={false}
+        minPolarAngle={Math.PI / 10}
+        maxPolarAngle={Math.PI / 2.7 }
+        maxAzimuthAngle={0.5* Math.PI}
+        minAzimuthAngle={2* Math.PI}
         enablePan={false}
+        enableZoom={false}
       />
     </Canvas>
   );
