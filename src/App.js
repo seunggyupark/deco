6e2e5c3e9a3664e4583a104c707f45a1e739b094
Dev <park@seung.dev>
2021-01-06 14:12:46 -0800

    Needed to patch-package react-spring conflicts.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.scss';
 import React, { useState } from 'react';
 
 import Header from './components/Header';
-import Loading from './components/UI/Loading'
 import Room from './components/Room';
 import ColorSelector from './components/ColorSelector/ColorSelector'
 
@@ -10,14 +9,13 @@ function App() {
   const [ pickedColor, setPickedColor ] = useState("");
 
   const pickedColorHandler = color => setPickedColor(color);
-  const url = "/bedroom.glb";
 
   return (
     <>
       <Header />
       <div className="page-container">
         <div className="room-container">
-          <Room pickedColor={pickedColor} url={url}/>
+          <Room pickedColor={pickedColor}/>
         </div>
         <ColorSelector pickedColor={pickedColor} pickedColorHandler={pickedColorHandler}/>
       </div>
